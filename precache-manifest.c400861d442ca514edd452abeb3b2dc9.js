f73d08b62dcd65a38c156523c0add260e4deb0db
Andrey Lyamkin <lyamkin.andr@gmail.com>
2020-09-02 12:12:38 -0400

    Updates

---
+++ precache-manifest.c400861d442ca514edd452abeb3b2dc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c92a1b7a0c3b2f5804cd5b45938f3ab",
+    "url": "/the-shoppies-react-app/index.html"
+  },
+  {
+    "revision": "dd805ecb2ce9d5ab2899",
+    "url": "/the-shoppies-react-app/static/css/main.54e250de.chunk.css"
+  },
+  {
+    "revision": "110dc745e7e8930b41e4",
+    "url": "/the-shoppies-react-app/static/js/2.ffd3990f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-shoppies-react-app/static/js/2.ffd3990f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd805ecb2ce9d5ab2899",
+    "url": "/the-shoppies-react-app/static/js/main.7021731c.chunk.js"
+  },
+  {
+    "revision": "a99afc91acd27dc95146",
+    "url": "/the-shoppies-react-app/static/js/runtime-main.0dca9cb2.js"
+  }
+]);
